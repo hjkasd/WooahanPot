e67061250ef020bf5bf590073563c9b87c7cc65d
hjkasd <75166072+hjkasd@users.noreply.github.com>
2020-11-29 18:52:30 +0900

    Update function34.ino

--- function34.ino
+++ function34.ino
@@ -1,5 +1,11 @@
-// 분갈이모듈
-int SW_POT = 13;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DS1302.h>
+
+int sw1 = 4; // 분갈이 스위치 4 번
+DS1302 rtc (5, 6, 7); // DS1302
+LiquidCrystal_I2C lcd (0x27,16,2);
+
 
 // play_music모듈 변수
 int BUZZER = 12; //스피커가 연결된 디지털핀 설정
@@ -14,20 +20,22 @@ int music[] = {3136,3136,2637,2093,2093,3136,3136,2793,2793,2349,2349,2349,2349,
 
 void setup() {
   Serial.begin(9600);
-  pinMode(SW_POT, INPUT_PULLUP);
+  // 새로운 날짜와 시간 정보를 칩에 저장
+  // rtc.writeProtect (false);
+  //rtc.halt(false);
+  // 시간 t (2020, 11, 28, 22, 30, 00, 시간 :: kSaturday);
+  //rtc.time(t);
+
+  lcd.begin (16, 2);
+  pinMode (sw1, INPUT_PULLUP);
   play_music();
 }
  
 void loop() {
-  // 분갈이 스위치 눌렀을 때
-  if(digitalRead(SW_POT) == LOW)
-    Serial.println(rtc.getDate());
-    Serial.println(rtc.getYear());
-    Serial.print();
-    //Serial.print(rtc.getMonth(),DEC);
-    //Serial.print(rtc.getDay(),DEC);
-   else
-    Serial.print("그만해줘")
+  if (digitalRead (sw1) == LOW) {
+printTime ();
+lcd.backlight ();
+}
    
 }
 
@@ -39,4 +47,13 @@ void play_music(){
     tone(BUZZER,music[i]/12,500);
     delay(350);
   }
+}
+
+void printTime () {
+Time t = rtc.time (); // 저장된 시간 읽어 오기
+// 날짜 출력
+char repot [12];
+snprintf (repot, sizeof (repot), "%04d-%02d-%02d", t.yr, t.mon, t.date);
+lcd.setCursor (0, 0);
+lcd.print (repot);
 }
